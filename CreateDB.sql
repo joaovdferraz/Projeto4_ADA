7568e916f89fec5de8be0b16b0104c84531928db
joaovdferraz <105398085+joaovdferraz@users.noreply.github.com>
2022-08-24 14:28:15 -0300

    Add files via upload

--- CreateDB.sql
+++ CreateDB.sql
@@ -4764,3 +4764,8 @@ VALUES
 (13, 146, '2022-07-30 14:13:40', 0),
 (5, 114, '2022-07-30 21:45:06', 1),
 (5, 114, '2022-07-30 23:11:06', 0);
+GO
+CREATE PROCEDURE cadastra_usuario @email VARCHAR(50), @senha VARCHAR(50), @cpf_usuario CHAR(11), @nome_usuario VARCHAR(50), @sobrenome_usuario VARCHAR(50), @numero_telefone CHAR(12), @data_nascimento DATE, @usuario_id INT AS INSERT INTO usuarios(senha_usuario,email_usuario)   VALUES(@senha,@email); 
+SET @usuario_id = SCOPE_IDENTITY(); INSERT INTO dados_pessoais(cpf_usuario,nome_usuario,sobrenome_usuario,numero_telefone,data_nascimento,usuario_id) VALUES(@cpf_usuario,@nome_usuario,@sobrenome_usuario,@numero_telefone,@data_nascimento,@usuario_id); 
+GO
+
